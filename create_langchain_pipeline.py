675f7ad67803eec3a286ef94dd1fd5a6817ba1c3
Yashkant Sharma <yashkantsharma64@gmail.com>
2025-09-01 18:57:13 +0530

    Update create_langchain_pipeline.py

--- create_langchain_pipeline.py
+++ create_langchain_pipeline.py
@@ -4,9 +4,9 @@ from langchain.chains import RetrievalQAWithSourcesChain
 def create_chain(llm, vector_store, map_prompt_template, combine_prompt_template):
     # create QA chain object using map_reduce method
     chain = RetrievalQAWithSourcesChain.from_chain_type(
-        llm=llm,
-        retriever=vector_store.as_retriever(search_kwargs={"k": 3}),
-        chain_type="map_reduce",
+        llm=llm, # pass the LLM instance
+        retriever=vector_store.as_retriever(search_kwargs={"k": 3}), # run semantic search to retrieve top k most similar chunks
+        chain_type="map_reduce", # use map reduce method to avoid token out of context error
         chain_type_kwargs={
             "question_prompt": map_prompt_template,
             "combine_prompt": combine_prompt_template,
