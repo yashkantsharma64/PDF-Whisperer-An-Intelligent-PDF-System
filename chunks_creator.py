5fa229f0ecb630b1eb8a98de6f7115c994ce28e1
Yashkant Sharma <yashkantsharma64@gmail.com>
2025-09-01 19:02:10 +0530

    Update chunks_creator.py

--- chunks_creator.py
+++ chunks_creator.py
@@ -4,9 +4,9 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 def create_chunks(docs):
     # create langchain text splitter object
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1500,
-        chunk_overlap=700,
-        add_start_index=True,
+        chunk_size=1500, # provide the size of the chunks
+        chunk_overlap=700, # tell the chunk overlap size
+        add_start_index=True, #to include start index of each chunk in the metadata
     )
     # split the text into chunks
     all_splits = text_splitter.split_documents(docs)
